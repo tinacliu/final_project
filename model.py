9b198e04cc8c883a9aca8384cedd39f84e785c4a
Tina Liu <tina.chang.liu@gmail.com>
2020-01-14 12:01:34 +0000

    refactored code for adding poly interactions and cross validation

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
-from sklearn.metrics import mean_squared_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, mean_squared_log_error, SCORERS
+
 
 from yellowbrick.datasets import load_concrete
 from yellowbrick.regressor import ResidualsPlot, PredictionError
@@ -15,7 +16,7 @@ import pandas as pd
 
 class ModelTry():
 
-  def __init__(self, X, y, test_size=.25):
+  def __init__(self, X, y, test_size=.3):
     self.X = X
     self.y = y
 
@@ -38,7 +39,7 @@ class ModelTry():
 
 
 
-  def data_transform(self, only_cont = False):
+  def data_transform(self, only_cont = False, poly = True):
 
     X_train , X_test, y_train, y_test = self.X_train , self.X_test, self.y_train, self.y_test
 
@@ -48,11 +49,23 @@ class ModelTry():
     X_test_con = X_test.loc[:,con_features]
 
     # Scale the train and test data
-    scaler = MinMaxScaler()
+    scaler = StandardScaler()
     scaler.fit(X_train_con)
 
     X_train_sca = scaler.transform(X_train_con)
     X_test_sca = scaler.transform(X_test_con)
+
+    if poly:
+    # add in the polynomial interations for the cont. variables
+      poly = PolynomialFeatures(2)
+      poly.fit(X_train_sca)
+      X_train_poly = poly.transform(X_train_sca)
+      X_test_poly = poly.transform(X_test_sca)
+      X_train_cols = poly.get_feature_names(X_train.columns)
+    else:
+      X_train_poly, X_test_poly = X_train_sca, X_test_sca
+      X_train_cols = X_train_con.columns
+
 
 
     # Create X_cat which contains only the categorical variables
@@ -72,8 +85,8 @@ class ModelTry():
     cat_train_df = pd.DataFrame(X_train_ohe.todense(), columns=columns)
     cat_test_df = pd.DataFrame(X_test_ohe.todense(), columns=columns)
 
-    X_train_all = pd.concat([pd.DataFrame(X_train_sca, columns=con_features), cat_train_df], axis = 1)
-    X_test_all = pd.concat([pd.DataFrame(X_test_sca, columns=con_features), cat_test_df], axis = 1)
+    X_train_all = pd.concat([pd.DataFrame(X_train_poly, columns=X_train_cols), cat_train_df], axis = 1)
+    X_test_all = pd.concat([pd.DataFrame(X_test_poly, columns=X_train_cols), cat_test_df], axis = 1)
 
     if only_cont:
       X_train_tr, X_test_tr = X_train_sca, X_test_sca
@@ -85,8 +98,18 @@ class ModelTry():
 
 
 
-  def run_model(self, model = 1, run_vis = False, only_cont = False, alpha = 1):
+  def run_model(self, model = 1, run_vis = False, poly = False, alpha = 1):
+    """
+    Intake: model code, run_visibility, poly, alpha level
+    :return: best_alpha
+    ------------------
+    model code - (1 is LinearRegression, 3 is Lasso, 4 is Ridge)
+
+    """
+    only_cont = False
     lin_reg = LinearRegression()
+
+
     if model == 1:
       only_cont = True
     elif model == 3:
@@ -94,7 +117,8 @@ class ModelTry():
     elif model == 4:
       lin_reg = Ridge(alpha)
 
-    X_train , X_test, y_train, y_test = self.data_transform(only_cont)
+
+    X_train , X_test, y_train, y_test = self.data_transform(only_cont, poly)
     lin_reg.fit(X_train, y_train)
 
     self.print_results(X_train, X_test, y_train, y_test, lin_reg)
@@ -116,9 +140,33 @@ class ModelTry():
       vis_pred.show();
 
 
-    return lin_reg, model_coef
+    return lin_reg, model_coef, X_train, y_train
 
 
+def grid_search(X_train, y_train, model, score = 'r2', cv=5):
+  """
+  Assumption: takes X, y train data to apply grid search to find best hyperparameter
+  based on given score measure.
+  :return: best_alpha
+
+  """
+
+  param_grid = [
+    {'alpha' : np.linspace(-2, 2, 81)
+    }
+  ]
+
+  gs_clf = GridSearchCV(model, param_grid = param_grid, scoring = score, cv=cv, return_train_score=True);
+  best_clf = gs_clf.fit(X_train, y_train)
+
+  bmodels = pd.DataFrame(best_clf.cv_results_).sort_values('rank_test_score')
+
+  for i in range(0,3):
+      bmodels[f'split{i}_drop'] = bmodels[f'split{i}_train_score']-bmodels[f'split{i}_test_score']
+
+  best_alpha = best_clf.best_params_['alpha']
+
+  return best_alpha, bmodels
 
 
 
